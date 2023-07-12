2e37615f3bbbd39edfc3e3c33a66a21766fda2f9
jhkim103 <mtrx9170@gmail.com>
2023-07-12 13:34:54 +0900

    feat(style): styleProvider 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,9 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <StyleProviderLayout>
-      <div>
-        <Title>MSP CS ADMIN</Title>
-        <p>powered by raidea.</p>
-      </div>
-    </StyleProviderLayout>
+    <div>
+      <Title>MSP CS ADMIN</Title>
+      <p>powered by raidea.</p>
+    </div>
   );
-}
-
-function StyleProviderLayout({ children }: { children: React.ReactNode }) {
-  const [cache] = useState(() => createCache());
-
-  const render = <>{children}</>;
-
-  useServerInsertedHTML(() => {
-    return (
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `</script>${extractStyle(cache)}<script>`,
-        }}
-      />
-    );
-  });
-
-  if (typeof window !== 'undefined') {
-    return render;
-  }
-
-  return <StyleProvider cache={cache}>{render}</StyleProvider>;
 }
