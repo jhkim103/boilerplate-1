62297f3e00488deef9ee49230aa10b446b67abc3
jhkim103 <mtrx9170@gmail.com>
2023-07-17 15:29:33 +0900

    fix(dx): eslint 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,77 +1,40 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:tailwindcss/recommended",
-    "plugin:import/typescript",
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  env: {
-    browser: true,
-    es2021: true,
-  },
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': 0,
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/destructuring-assignment': [0, 'always'],
+    'import/extensions': 'off',
+    'import/order': [
+      'error',
       {
-        endOfLine: "auto",
-      },
-    ],
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "react/jsx-filename-extension": ["off", { allow: "as-needed" }],
-    "react/react-in-jsx-scope": "off",
-    "react/prefer-stateless-function": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/require-default-props": "off",
-    "jsx-a11y/label-has-associated-control": "off",
-    "import/prefer-default-export": "off",
-    "react/jsx-no-useless-fragment": "warn",
-    "import/no-unresolved": "off",
-    "import/extensions": "off",
-    "import/order": [
-      "error",
-      {
-        groups: ["builtin", "external", ["parent", "sibling"], "index"],
-        pathGroups: [
-          {
-            pattern: "angular",
-            group: "external",
-            position: "before",
-          },
-        ],
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
-        "newlines-between": "always",
+        'newlines-between': 'never',
       },
     ],
-    "import/no-extraneous-dependencies": "off", // pnpm이랑 충돌 (?)
   },
   settings: {
-    "import/resolver": {
-      node: {
-        moduleDirectory: ["node_modules", "src/"],
+    'import/resolver': {
+      node: {},
+      typescript: {
+        directory: './src',
       },
-    },
-    tailwindcss: {
-      config: "packages/apps/prototype-mk1/tailwind.config.cjs",
     },
   },
 };
--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -2,7 +2,6 @@
 
 import { Typography } from 'antd';
 import { PropsWithChildren } from 'react';
-import type { TypographyProps } from 'antd';
 
 export default function Title(props: PropsWithChildren) {
   return <Typography.Title>{props.children}</Typography.Title>;
