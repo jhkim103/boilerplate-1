c0192e9937da4884102ce43b595273672d0fcf23
jhkim103 <mtrx9170@gmail.com>
2023-07-19 13:27:02 +0900

    fix(antd): paragraph 수정

--- src/app/components/Paragraph/index.tsx
+++ src/app/components/Paragraph/index.tsx
@@ -3,11 +3,16 @@
 import { Typography } from 'antd';
 import { ParagraphProps } from 'antd/es/typography/Paragraph';
 import { ReactNode } from 'react';
+import StyledComponentsRegistry from '@bp1/app/provider/StyledComponentsRegistry';
 
 interface ITypographyProps extends ParagraphProps {
   children?: ReactNode | string;
 }
 
 export default function Title(props: ITypographyProps) {
-  return <Typography.Paragraph {...props}>{props.children}</Typography.Paragraph>;
+  return (
+    <StyledComponentsRegistry>
+      <Typography.Paragraph {...props}>{props.children}</Typography.Paragraph>
+    </StyledComponentsRegistry>
+  );
 }
