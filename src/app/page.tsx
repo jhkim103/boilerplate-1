451c1515bfad8f823289440b3f2e431aad9ef3bc
jhkim103 <mtrx9170@gmail.com>
2023-07-24 15:11:25 +0900

    feat(route): Parallel Routes->page변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
 'use client';
 
-// import { Button, Col, Row, Typography } from '../components/antd';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div>
-      {/* <Typography.Title level={1}>MSP CS ADMIN</Typography.Title>
-      <Row gutter={[16, 16]}>
-        <Col>
-          <Button rdsType="test" type="dashed">
-            test1
-          </Button>
-        </Col>
-        <Col>
-          <Button type="primary">test1</Button>
-        </Col>
-        <Col>
-          <Button>test2</Button>
-        </Col>
-      </Row> */}
-
-      <p>powered by raidea.</p>
-    </div>
-  );
+  const isLogin = false;
+  const route = useRouter();
+  if (isLogin) {
+    route.push('/dashboard');
+  } else {
+    route.push('/login');
+  }
 }
